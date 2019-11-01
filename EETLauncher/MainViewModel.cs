443d63f0656b68c978bcc184c79a4f8e883065dd
ALIENQuake <alienquake@hotmail.com>
2019-11-01 14:46:16 +0100

    cosmetic

--- EETLauncher/MainViewModel.cs
+++ EETLauncher/MainViewModel.cs
@@ -10,27 +10,27 @@ namespace EETLauncherMVVM {
         public bool _enabled;
         public Visibility _logVisibility;
 
-        public ICommand PlayEET {get;}
-        public ICommand OpenEETReadMe {get;}
-        public ICommand OpenEETHomePage {get;}
-        public ICommand OpenModManagerPage {get;}
-        public ICommand CheckForUpdates {get;}
-        public ICommand Exit {get;}
+        public ICommand PlayEET { get; }
+        public ICommand OpenEETReadMe { get; }
+        public ICommand OpenEETHomePage { get; }
+        public ICommand OpenModManagerPage { get; }
+        public ICommand CheckForUpdates { get; }
+        public ICommand Exit { get; }
 
         public MainViewModel() {
 
             Enabled = true;
             LogVisibility = Visibility.Hidden;
 
-            PlayEET = ReactiveCommand.Create( PlayEET_OnExecuted );
-            OpenEETReadMe = ReactiveCommand.Create( OpenEETReadMe_OnExecuted );
-            OpenEETHomePage = ReactiveCommand.Create( OpenEETHomePage_OnExecuted );
-            OpenModManagerPage = ReactiveCommand.Create( OpenModManagerPage_OnExecuted );
-            CheckForUpdates = ReactiveCommand.Create( CheckForUpdates_OnExecuted );
-            Exit = ReactiveCommand.Create( Exit_OnExecuted );
+            PlayEET = ReactiveCommand.Create(PlayEET_OnExecuted);
+            OpenEETReadMe = ReactiveCommand.Create(OpenEETReadMe_OnExecuted);
+            OpenEETHomePage = ReactiveCommand.Create(OpenEETHomePage_OnExecuted);
+            OpenModManagerPage = ReactiveCommand.Create(OpenModManagerPage_OnExecuted);
+            CheckForUpdates = ReactiveCommand.Create(CheckForUpdates_OnExecuted);
+            Exit = ReactiveCommand.Create(Exit_OnExecuted);
 
         }
-        
+
         public bool Enabled {
             get => _enabled;
             set => this.RaiseAndSetIfChanged(ref _enabled, value);
@@ -46,20 +46,20 @@ namespace EETLauncherMVVM {
         }
 
         public void PlayEET_OnExecuted() {
-            Process.Start( AppRootPath + GameExeFileName );
+            Process.Start(AppRootPath + GameExeFileName);
             Application.Current.Shutdown();
         }
 
         public void OpenEETReadMe_OnExecuted() {
-            Process.Start( AppRootPath + EETReadMeFilePath );
+            Process.Start(AppRootPath + EETReadMeFilePath);
         }
 
         public void OpenModManagerPage_OnExecuted() {
-            Process.Start( ModManagerHomePage );
+            Process.Start(ModManagerHomePage);
         }
 
         public void OpenEETHomePage_OnExecuted() {
-            Process.Start( EETHomePage );
+            Process.Start(EETHomePage);
         }
 
         public void Exit_OnExecuted() {
