cc6d088e25fc7002fb94a7f589a7816d78d004d9
ALIENQuake <alienquake@hotmail.com>
2019-12-13 10:16:28 +0100

    ConfigureAwait should be default(true) for GUI aps

--- EETLauncher/SettingsViewModel.cs
+++ EETLauncher/SettingsViewModel.cs
@@ -82,7 +82,7 @@ namespace EETLauncherMVVM {
                             EETGuiProcess = null;
                         }
                         return EETGuiProcess;
-                    }).ConfigureAwait(false)) {
+                    })) {
                         if (result == null) return;
                     }
 
