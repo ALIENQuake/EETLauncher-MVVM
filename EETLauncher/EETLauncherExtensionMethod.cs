26ca9131b466f3e457a1f4a6f9ddf75e69e4c573
ALIENQuake <alienquake@hotmail.com>
2020-08-17 18:26:19 +0200

    cosmetic

--- EETLauncher/EETLauncherExtensionMethod.cs
+++ EETLauncher/EETLauncherExtensionMethod.cs
@@ -2,7 +2,7 @@
 using System;
 
 namespace EETLauncherMVVM {
-    public static class EETLauncherStringExtensions {
+    public static class EETLauncherExtensionMethod {
         // Why this is not build-in method of the .NET?
         public static bool ContainsIgnoreCase(string source, string toCheck) {
             return source?.IndexOf(toCheck, StringComparison.OrdinalIgnoreCase) >= 0;
